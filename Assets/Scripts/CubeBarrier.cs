693e9bc65568f1097f8752fb9416eb7410e6760d
Fastermate <andreyyerokhin@mail.ru>
2022-08-24 12:29:24 +0700

    Add materials

--- Assets/Scripts/CubeBarrier.cs
+++ Assets/Scripts/CubeBarrier.cs
@@ -7,9 +7,18 @@ public class CubeBarrier : MonoBehaviour
 {
     [SerializeField] public int Value;
     [SerializeField] public TextMeshPro ValueText;
+    Renderer rend;
 
     private void Update()
     {
         ValueText.SetText(Value.ToString());
+        rend.material.SetFloat("_Step", Value * 0.01f);
+    }
+
+    private void Awake()
+    {
+        rend = GetComponent<Renderer>();
+        
+        
     }
 }
